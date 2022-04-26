b7fd8773cd1906a37486f8d5520056622ad16417
dkswjdrbs123 <dkswjdrbs12@naver.com>
2022-04-26 17:18:05 +0900

    영화 검색 조회 개수 필터 로직 추가

--- src/store/movie.js
+++ src/store/movie.js
@@ -18,7 +18,7 @@ export default {
     mutations: {
         updateState(state, payload) {
 
-            // ['movies', 'message', 'loading']
+            // ['movies', 'message', 'loading', ...]
             Object.keys(payload).forEach(key => {
                 // state.message = payload.message
                 // state.movies = payload.movies
@@ -33,7 +33,7 @@ export default {
     },
     //비동기로 동작
     actions: {
-        async searchMovies({commit}, payload) {
+        async searchMovies({commit, state}, payload) {
             const {title, type, number, year} = payload;
 
             const OMDB_API_KEY = '7035c60c'
@@ -41,9 +41,26 @@ export default {
             const { Search, totalResults} = res.data
             commit('updateState', {
                 movies: Search
-                // message: 'Hello world',
-                // loading: true
             })
+            console.log(res.data);
+            console.log(totalResults);
+            console.log(typeof totalResults);
+
+            const total = parseInt(totalResults,10);
+            const pageLength = Math.ceil(total /10);
+
+            //추가 요청
+            if(pageLength > 1) {
+                for(let page = 2; page <= pageLength; page += 1){                    
+                    if(page > (number / 10)) break;
+
+                    const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)                    
+                    const { Search } = res.data
+                    commit('updateState', {
+                        movies: [...state.movies, ...Search] //전개 연산자?
+                    })
+                }
+            }
         }
     }
 }
